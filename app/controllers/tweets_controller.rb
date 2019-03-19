df77b3aa608a14b7ba2942d25f83ceaa12aa8c33
Samantha brake <sammy.brake@gmail.com>
2019-03-19 13:40:15 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -46,8 +46,8 @@ get '/tweets/:id' do
   end
 end
 
-### just writing out future routes
 get '/tweets/:id/edit' do
+  binding.pry
   @user = User.find_by(session[:user_id])
   if @user
     @tweet = Tweet.find_by(params[:id])
